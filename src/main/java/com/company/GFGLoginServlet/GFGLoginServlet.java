b48226eb36a9e84c4b92be8e9530f3abb5efc328
Aman123456h <jaman0120@gmail.com>
2023-09-16 11:49:08 +0530

    Initial commit

--- src/main/java/com/company/GFGLoginServlet/GFGLoginServlet.java
+++ src/main/java/com/company/GFGLoginServlet/GFGLoginServlet.java
@@ -59,7 +59,7 @@ public class GFGLoginServlet extends HttpServlet {
 						HttpServletResponse response)
 		throws ServletException, IOException
 	{
-		// TODO Auto-generated method stub
+		
 		doGet(request, response);
 	}
 }
